9ea9aea421a3cbff66e57cb4c8ddf16777333e71
Amen <kluverkamp@hotmail.com>
2014-05-03 17:42:25 +0200

    add chat client based on socket.io

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,24 @@
+$(window).load(function() {
+	
+	var socket = io.connect('http://the-dell-pc:3000');
+
+	socket.on('good', function (data) {
+		console.log(data);
+		$("#messages").append("<b>" + data.username + "</b>: " + data.message + "<br />");
+	});
+
+	$("#message").keypress(function (e) {
+  		if (e.which == 13) {
+    		$("#myButton").click();
+  		}
+	});
+
+	$("#myButton").click(function(event) {
+		socket.emit('click', {
+			username: $("#username").val(),
+			message: $("#message").val()
+		});
+		$("#message").val("");
+	});
+});
 
