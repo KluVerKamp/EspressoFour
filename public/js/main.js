5b7a93a3662bde76605676f9cc824a097e5bf0f7
Amen <amen@emworks.com>
2014-06-19 09:34:17 +0100

    Sound notification

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,7 @@
+var focus = 1;
+var audio = new Audio('/audio/hangout_dingtone.m4a')
+audio.volume = 0.3
+
 $(window).load(function() {
 	
 	var socket = io.connect(myIP);
@@ -5,22 +9,46 @@ $(window).load(function() {
 	socket.on('good', function (data) {
 		console.log(data);
 		$("#messages").append("<b>" + data.username + "</b>: " + data.message + "<br />");
+		if(!focus)
+			audio.play()
 	});
 
 	$("#message").keypress(function (e) {
-  		if (e.which == 13 && $("#message").val()!="") {
+  		if (e.which == 13 ) {
     		$("#myButton").click();
   		}
 	});
 
 	$("#myButton").click(function(event) {
-		socket.emit('click', {
-			username: $("#username").val(),
-			message: $("#message").val()
-		});
+		
+		if($("#username").val()&&$("#message").val()){
+			socket.emit('click', {
+				username: $("#username").val(),
+				message: $("#message").val()
+			});
 
-		$("#message").val("");
+			$("#message").val("");
+			
+			$("#username").prop('disabled', true);
 
-		$("#messages").scrollTop($("#messages")[0].scrollHeight);
+			$("#messages").scrollTop($("#messages")[0].scrollHeight);
+		}
 	});
 });
+
+
+$(window).on("blur focus", function(e) {
+    var prevType = $(this).data("prevType");
+    if (prevType != e.type) {   //  reduce double fire issues
+        switch (e.type) {
+            case "blur":
+               focus = 0;
+                break;
+            case "focus":
+                focus = 1;
+                break;
+        }
+    }
+
+    $(this).data("prevType", e.type);
+})
